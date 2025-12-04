081fe6b4d863ecbeb5da406080213cb8c3aa9864
Michael Wender <michael@michaelwender.com>
2025-12-04 10:07:39 -0500

    Running Prettier

--- src/client-sync.ts
+++ src/client-sync.ts
@@ -125,7 +125,7 @@ export default async function main(props: LaunchProps<{ launchContext?: ClientSy
     await showHUD(
       `Synced ${clients.length} client${clients.length === 1 ? "" : "s"}${
         includeInactive ? " (including inactive)" : ""
-      } ✅`
+      } ✅`,
     );
   } catch (error) {
     console.error(error);
